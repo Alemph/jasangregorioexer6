dcd89a7a6fdeab1b230d64b09073b56e46a5c846
Alemph <jlsangregorio@up.edu.ph>
2024-03-14 15:55:13 +0800

    debugging

--- mongodb.js
+++ mongodb.js
@@ -2,37 +2,23 @@ import mongoose from 'mongoose';
 
 
 // connection string
-await mongoose.connect('mongodb://127.0.0.1:27017/ICS', { useNewUrlParser: true, useUnifiedTopology: true }); 
+await mongoose.connect('mongodb+srv://jlsangregorio:sNmqK9aYzUooYQZA@cluster0.na6nkrh.mongodb.net/ICS');
 
-const Student = mongoose.model('Subject', {
-    stdnum: String,
+const Student = mongoose.model('students', {
+    stdnum: Number,
     fname: String,
     lname: String,
     age: Number
-  }, 'studentData')
+  })
 
-  // the result parameter will contain a single object (the first matching document found
-// if no matching document was found, result will be null
-// let data = await Student.findOne({ fname: “Peter”, lname: “Parker” });
-// console.log(data);
-
-// results here will always be an array, regardless of how many matching documents were found
 let data = await Student.find({ age: 17 });
 console.log(data);
 
 const newStudent = new Student({
-    stdnum: "12345678",
+    stdnum: 12345678,
     fname: "Juan",
     lname: "dela Cruz",
-    age: 20
+    age: 17
 });
 
 await newStudent.save();
-
-let stud = await Student.findOne({ age: 65});
-stud.fname = "Peter"
-await stud.save();
-
-await Student.deleteOne({ stdnum: '8051495845'})
-
-await Student.deleteMany({ fname: 'John' })
