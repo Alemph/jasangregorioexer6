2e48bc8125bc85e6f674dab5d174c3a81ee14c40
Alemph <jlsangregorio@up.edu.ph>
2024-04-18 06:25:50 +0800

    changed some of request.js

--- request.js
+++ request.js
@@ -3,7 +3,7 @@ import needle from 'needle';
 const baseURL = 'http://localhost:3000';
 
 // Add a new student, "Mary Jane Watson", to the database.
-const testSaveStudent = async () => {
+const saveStudent = async () => {
     const payload = {
         stdnum: 8051495845,
         fname: 'Mary Jane',
@@ -15,13 +15,13 @@ const testSaveStudent = async () => {
 };
 
 // Update the last name of "Mary Jane" to "Parker".
-const testUpdate = async () => {
+const update = async () => {
     const response = await needle('post', `${baseURL}/update`, null, { json: true });
     console.log('Update:', response.body);
 };
 
 // Remove the student record with student number.
-const testRemoveUser = async () => {
+const removeUser = async () => {
     const payload = {
         stdnum: 8051495845
     };
@@ -30,28 +30,28 @@ const testRemoveUser = async () => {
 };
 
 // Remove all students' details from the database.
-const testRemoveAllUsers = async () => {
+const removeAllUsers = async () => {
     const response = await needle('post', `${baseURL}/remove-all-user`, null, { json: true });
     console.log('Remove All Users:', response.body);
 };
 
 // Retrieve details for the student with student number.
-const testUser = async () => {
+const user = async () => {
     const stdnum = '8051495845';
     const response = await needle('get', `${baseURL}/user?stdnum=${stdnum}`);
     console.log('User:', response.body);
 };
 
 // Retrieve details for all students.
-const testMembers = async () => {
+const members = async () => {
     const response = await needle('get', `${baseURL}/members`);
     console.log('Members:', response.body);
 };
 
 // Uncomment the following lines to execute the corresponding functions:
-testSaveStudent();
-testUpdate();
-// testRemoveUser();
-// testRemoveAllUsers();
+saveStudent();
+update();
+// removeUser();
+// removeAllUsers();
 testUser();
 testMembers();
